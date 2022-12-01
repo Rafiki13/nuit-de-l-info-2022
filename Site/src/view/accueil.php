7b682cd7916cd64ba18807351eddd4ac180df535
HellieRoses <maeboissezon@gmail.com>
2022-12-01 23:28:19 +0100

    avancé css accueil

--- Site/src/view/accueil.php
+++ Site/src/view/accueil.php
@@ -1,21 +1,21 @@
 <main>
 
-    <div id="intro">
+    <div class="arttexte">
         <h1>Bienvenue sur ISTDefenseTheGame !</h1>
         <p>Le but de ce site est de vous sensibiliser sur le sida tout en vous divertissant.zeikyqcfvlvhfhsdfcvlsvhyldsvbldscbv</p>
     </div>
 
     <div class="img">
-        <img src="assets/img/jeuexemple.jpg" allt="jeuexemple">
+        <img src="assets/img/jeuexemple.jpg" alt="jeuexemple">
         <p>Lore du jeu</p>
     </div>
 
     <div class="img">
-        <img src="assets/img/jeuexemple.jpg" allt="jeuexemple">
+        <img src="assets/img/jeuexemple.jpg" alt="jeuexemple">
         <p>Bestiaire et explications</p>
     </div>
 
-    <div>
+    <div class="arttexte">
         <h2>Contacts importants</h2>
         <ul>
         <p>Voici une liste non-exaustive des différents endroits et téléphones pour trouver plus d'informations:</p>
