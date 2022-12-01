5738e536d7c87ac5ba748cef59e35daf0e07aede
AriakeTM <teo.moerelpro@gmail.com>
2022-12-01 22:16:22 +0100

    chgmt accueil 1

--- Site/src/view/accueil.php
+++ Site/src/view/accueil.php
@@ -1,11 +1,13 @@
 <header>
-    <blockquote><strong>“Protégez-vous !”</strong></blockquote>
-    <cite>L'équipe J'ai perdu</cite>
-</header>
+        <blockquote><strong>“Protégez-vous !”</strong></blockquote>
+        <cite>L'équipe J'ai perdu</cite>
+    </header>
 
 <main>
     <div>
         <div>
+            <div>Bienvenue sur ISTDefenseTheGame ! Le but de ce site est de vous sensibiliser sur le sida tout en vous divertissant.</div>
+            <img id="" src="" alt="Image du Jeu">
             
         </div>
     </div>
