f4eb05276cd959e6d2e8f832abd4057a4fc3676c
HellieRoses <maeboissezon@gmail.com>
2022-12-01 19:44:27 +0100

    début html bestiaire

--- Site/src/view/bestiaire.php
+++ Site/src/view/bestiaire.php
@@ -1,4 +1,26 @@
 <main>
     <h1> Bestiaire</h1>
-    <p> Bienvenue aux bestiaires, ici se trouve toutes les dif</p>
+    <p>Bienvenue aux bestiaires, ici se trouve toutes les différentes IST et maladies iojdfshfioh doifchv</p>
+    <div>
+        <h2>Celles présentes dans le jeu</h2>
+            <div>
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+            </div>
+        <h2>Ils en existent d'autres !</h2>
+            <div>
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+                <img src="![](../../web/assets/img/carteprovi.png)" alt="hgxfcir">
+            </div>
+    </div>
 </main>
