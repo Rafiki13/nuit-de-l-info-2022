adc955a90b131800ff1219ccc694908b82ac99ef
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-12-02 01:33:17 +0100

    bestiaire dynamique

--- Site/src/view/bestiaire.php
+++ Site/src/view/bestiaire.php
@@ -2,96 +2,59 @@
 <p>Bienvenue aux bestiaires, ici se trouve toutes les différentes IST et maladies iojdfshfioh doifchv</p>
 <div id="bestiaire">
     <h2>Celles présentes dans le jeu</h2>
-        <div class="cardContainer">
-            <div class="card">
-                <div class="front">
-                    <img src="assets/img/carteprovi.png" alt="son p'tit nom"> <!--TODO mettre la bonne image-->
-                    <div class="cardHead">
-                        <h2>Son p'tit nom</h2>
-                        <div class="dangerosite">
-                            <div class="active"></div>
-                            <div class="active"></div>
-                            <div></div>
+    <div class="cardContainer">
+        <?php
+            require_once __DIR__ . '/../Model/dataObject/Ist.php';
+
+            foreach(\App\Ndi\Model\DataObject\Ist::getInGame() as $ist){
+                echo <<<HTML
+                    <div class="card">
+                        <div class="front">
+                            <img src="{$ist->getCheminImage()}" alt="{$ist->getAltImage()}">
+                            <div class="cardHead">
+                                <h2>{$ist->getNom()}</h2>
+                                <div class="dangerosite">
+                                    {$ist->getNiveauDanger()}
+                                </div>
+                            </div>
+                            <p class="type">{$ist->getType()}</p>
+                        </div>
+                        <div class="back">
+                            <h2>Se protéger</h2>
+                            <p>{$ist->getSeProteger()}</p>
+                            <h2>Traitements</h2>
+                            <p>{$ist->getTraitements()}</p>
                         </div>
                     </div>
-
-                    <p class="type">virus</p>
-                </div>
-
-                <div class="back">
-                    <h2>Se protéger</h2>
-                    <p>description</p>
-                    <h2>Traitements</h2>
-                    <p>liste traitements</p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="front">
-                    <img src="assets/img/carteprovi.png" alt="son p'tit nom"> <!--TODO mettre la bonne image-->
-                    <div class="cardHead">
-                        <h2>Son p'tit nom</h2>
-                        <div class="dangerosite">
-                            <div class="active"></div>
-                            <div class="active"></div>
-                            <div></div>
-                        </div>
-                    </div>
-
-                    <p class="type">virus</p>
-                </div>
-
-                <div class="back">
-                    <h2>Se protéger</h2>
-                    <p>description</p>
-                    <h2>Traitements</h2>
-                    <p>liste traitements</p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="front">
-                    <img src="assets/img/carteprovi.png" alt="son p'tit nom"> <!--TODO mettre la bonne image-->
-                    <div class="cardHead">
-                        <h2>Son p'tit nom</h2>
-                        <div class="dangerosite">
-                            <div class="active"></div>
-                            <div class="active"></div>
-                            <div></div>
-                        </div>
-                    </div>
-
-                    <p class="type">virus</p>
-                </div>
-
-                <div class="back">
-                    <h2>Se protéger</h2>
-                    <p>description</p>
-                    <h2>Traitements</h2>
-                    <p>liste traitements</p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="front">
-                    <img src="assets/img/carteprovi.png" alt="son p'tit nom"> <!--TODO mettre la bonne image-->
-                    <div class="cardHead">
-                        <h2>Son p'tit nom</h2>
-                        <div class="dangerosite">
-                            <div class="active"></div>
-                            <div class="active"></div>
-                            <div></div>
-                        </div>
-                    </div>
-
-                    <p class="type">virus</p>
-                </div>
-
-                <div class="back">
-                    <h2>Se protéger</h2>
-                    <p>description</p>
-                    <h2>Traitements</h2>
-                    <p>liste traitements</p>
-                </div>
-            </div>
-        </div>
+                HTML;
+            }
+        ?>
+    </div>
     <h2>Il en existe d'autres !</h2>
-
+    <div class="cardContainer">
+        <?php
+        foreach(\App\Ndi\Model\DataObject\Ist::getNotInGame() as $ist){
+            echo <<<HTML
+                    <div class="card">
+                        <div class="front">
+                            <img src="{$ist->getCheminImage()}" alt="{$ist->getAltImage()}">
+                            <div class="cardHead">
+                                <h2>{$ist->getNom()}</h2>
+                                <div class="dangerosite">
+                                    {$ist->getNiveauDanger()}
+                                </div>
+                            </div>
+                            <p class="type">{$ist->getType()}</p>
+                        </div>
+                        <div class="back">
+                            <h2>Se protéger</h2>
+                            <p>{$ist->getSeProteger()}</p>
+                            <h2>Traitements</h2>
+                            <p>{$ist->getTraitements()}</p>
+                        </div>
+                    </div>
+                HTML;
+        }
+        ?>
+    </div>
 </div>
