b21e58eebdb8819fcf1d3f9c5041baa4f2f3c336
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-12-02 05:59:53 +0100

    transitions capotes

--- Site/src/view/accueil.php
+++ Site/src/view/accueil.php
@@ -1,42 +1,59 @@
-<main>
+<input type="checkbox" id="openPreservatifs"/>
+<div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+    <div class="preservatif"></div>
+</div>
 
-    <div class="arttexte" id="intro">
-        <div>
-        <h1>Bienvenue sur MSTower !</h1>
-        <img src="assets/img/logoSida.png" alt="logosidaction">
-        </div>
-        <p>Le but de ce site est de vous sensibiliser sur le sida tout en vous divertissant. Bonne navigation!</p>
+<div class="arttexte" id="intro">
+    <div>
+    <h1>Bienvenue sur MSTower !</h1>
+        <label for="openPreservatifs">
+            <img src="assets/img/logoSida.png" alt="logosidaction">
+        </label>
+    </div>
+    <p>Le but de ce site est de vous sensibiliser sur le sida tout en vous divertissant. Bonne navigation!</p>
+</div>
+
+
+    <div class="img" id="jeu">
+        <a href="frontController.php?action=jeu" href="aller sur la page du jeu">
+        <div><p>Commencer le jeu</p></div>
+        </a>
+        <p>Bienvenue dans MSTower. Votre mission est simple, protéger le royaume d’Aphrodisa de la légion vénérienne.
+            Triomphez sur 3 niveaux d’une dizaine de vagues d’ennemis successifs à l’aide la puissante armée de votre royaume !</p>
     </div>
 
 
-        <div class="img" id="jeu">
-            <a href="frontController.php?action=jeu" href="aller sur la page du jeu">
-            <div><p>Commencer le jeu</p></div>
-            </a>
-            <p>Bienvenue dans MSTower. Votre mission est simple, protéger le royaume d’Aphrodisa de la légion vénérienne.
-                Triomphez sur 3 niveaux d’une dizaine de vagues d’ennemis successifs à l’aide la puissante armée de votre royaume !</p>
-        </div>
 
-
-
-        <div class="img" class="particle" id="bestiaire">
-            <a href="frontController.php?action=bestiaire" href="aller sur la page du Bestiaire">
-            <div><p>Découvrir les MST</p></div>
-            </a>
-            <p>Le bestiaire est un recueil regroupant quelques IST présentes dans la vie et qui ont été retranscrites dans le jeu.
-                Elles ont toutes un nom, une description de leur effet sur l’humain, et une description dans le jeu et leur faiblesse. Il peut donc être très intéressant de
-                lire cet ISTdex pour se renseigner, ne serait-ce que pour réussir notre jeu !</p>
-        </div>
-
-
-    <div class="arttexte">
-        <h2>Contacts importants</h2>
-        <p>Voici une liste non-exaustive des différents endroits et téléphones pour trouver plus d'informations:</p>
-        <ul>
-            <li><a href="https://www.ameli.fr/assure/sante/themes/mst-ist/depistage">https://www.ameli.fr/assure/sante/themes/mst-ist/depistage</a></li>
-            <li><a href="https://sida-info-services.org">https://sida-info-services.org</a></li>
-            <li><a href="https://www.planning-familial.org/fr">https://www.planning-familial.org/fr</a></li>
-            <li><p>Sida Info Service: <a href="tel:0800840800">0 800 840 800</a></p></li>
-        </ul>
+    <div class="img" class="particle" id="bestiaire">
+        <a href="frontController.php?action=bestiaire" href="aller sur la page du Bestiaire">
+        <div><p>Découvrir les MST</p></div>
+        </a>
+        <p>Le bestiaire est un recueil regroupant quelques IST présentes dans la vie et qui ont été retranscrites dans le jeu.
+            Elles ont toutes un nom, une description de leur effet sur l’humain, et une description dans le jeu et leur faiblesse. Il peut donc être très intéressant de
+            lire cet ISTdex pour se renseigner, ne serait-ce que pour réussir notre jeu !</p>
     </div>
-</main>
+
+
+<div class="arttexte">
+    <h2>Contacts importants</h2>
+    <p>Voici une liste non-exaustive des différents endroits et téléphones pour trouver plus d'informations:</p>
+    <ul>
+        <li><a href="https://www.ameli.fr/assure/sante/themes/mst-ist/depistage">https://www.ameli.fr/assure/sante/themes/mst-ist/depistage</a></li>
+        <li><a href="https://sida-info-services.org">https://sida-info-services.org</a></li>
+        <li><a href="https://www.planning-familial.org/fr">https://www.planning-familial.org/fr</a></li>
+        <li><p>Sida Info Service: <a href="tel:0800840800">0 800 840 800</a></p></li>
+    </ul>
+</div>
